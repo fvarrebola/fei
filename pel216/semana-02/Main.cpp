f2f7e9f917294b61371a33f874b6f67265a48f27
fvarrebola <fvarrebola@gmail.com>
2014-06-30 22:43:29 -0300

    [PEL216]: Ajuste da classe principal da semana 02.

--- pel216/semana-02/Main.cpp
+++ pel216/semana-02/Main.cpp
@@ -8,28 +8,15 @@
 using namespace pel216::commons;
 using namespace pel216::week2;
 
-void playWithShapes();
-
 // enum de comandos
 enum Commands {
-    PLAY_WITH_SHAPES    			= 0,
-    RUN_TESTS                      = 1,
-    DUMP                           = 10,
-    PUSH                           = 11,
-    POP                            = 12,
-    PEEK                           = 13,
-    COUNT                          = 14,
-    CLEAR                          = 15,
+    PLAY_WITH_SHAPES 				= 0,
+    RUN_TESTS                      	= 1,
     EXIT                           = 99
 };
 
 /* menu principal */
 MENU_OPTION MAIN_MENU[] = {
-	{
-		PLAY_WITH_SHAPES,
-		"Usando formas geometricas",
-		playWithShapes
-	},
 	{
 		RUN_TESTS,
 		"Executar testes",
@@ -42,28 +29,6 @@ MENU_OPTION MAIN_MENU[] = {
     },
     {-1, NULL, NULL}
 };
-
-/* menu de operacoes sobre formas */
-MENU_OPTION SUBMENU[] = {
-    {DUMP,   "Exibe",           NULL},
-    {PUSH,   "Insere (push)",   NULL},
-    {POP,    "Remove (pop)",    NULL},
-    {PEEK,   "Espia  (peek)",   NULL},
-    {COUNT,  "Conta  (count)",  NULL},
-    {CLEAR,  "Limpa  (clear)",  NULL},
-    {EXIT,   "Sair",            NULL},
-    {-1, NULL, NULL}
-};
-
-
-
-
-/**
- * Metodo que ilustra o uso de formas geometricas.
- */
-void playWithShapes() {
-
-}
 
 
 
