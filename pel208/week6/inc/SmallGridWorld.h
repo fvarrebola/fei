613779a3a295a8e0b81cda47ff43faa20b791156
Fabio Arrebola <fvarrebola@gmail.com>
2014-11-23 21:37:25 -0200

    [PEL208]: Inclusão do projeto week6.

---
+++ pel208/week6/inc/SmallGridWorld.h
@@ -0,0 +1,192 @@
+#pragma once
+
+#ifndef __SMALL_GRID_WORLD_H__
+#define __SMALL_GRID_WORLD_H__
+
+#include <vector>
+
+#include <inc\Utils.h>
+#include <inc\ex\IllegalParameterException.h>
+
+#include <inc\SmallGridWorldState.h>
+
+using namespace pel216::commons;
+
+namespace pel208 {
+
+	namespace week6 {
+
+		/**
+		 * Classe que representa o <i>small grid world</i>.<br />
+		 *
+		 * @author arrebola
+		 */
+		class SmallGridWorld {
+
+		private:
+			size_t rows;
+			size_t stateCount;
+			std::vector<SmallGridWorldState*> *states;
+
+			/**
+			 * Constrói a matriz de próximos estados.<br />
+			 *
+			 * @param stateIdx
+			 *					o <code>size_t</code> que representa o índice do estado atual
+			 *
+			 * @return o Matrix que representa a matriz de próximos estados
+			 */
+			PRIVATE Matrix *buildNextStatesCompleteInfo(IN size_t stateIdx) {
+				
+				Matrix *matrix = new Matrix(NEXT_STATE_COMPLETE_INFO_MATRIX_ROWS, ALLOWED_ACTIONS_QTY);
+
+				// configura os próximos estados (up, down, left, right)
+				matrix->data()[0][0] = (stateIdx < ALLOWED_ACTIONS_QTY) ? stateIdx : (stateIdx - ALLOWED_ACTIONS_QTY);
+				matrix->data()[0][1] = (stateIdx < this->stateCount - ALLOWED_ACTIONS_QTY) ? stateIdx + ALLOWED_ACTIONS_QTY : stateIdx;
+				matrix->data()[0][2] = (stateIdx % ALLOWED_ACTIONS_QTY == 0) ? stateIdx : stateIdx - 1;
+				matrix->data()[0][3] = (stateIdx % ALLOWED_ACTIONS_QTY == 3) ? stateIdx : stateIdx + 1;
+
+				// configura as probabilidades de transição e as recompensas
+				for (size_t idx = 0; idx < ALLOWED_ACTIONS_QTY; idx++) {
+					matrix->data()[1][idx] = 0.25f;
+					matrix->data()[2][idx] = -1.0f;
+					if (stateIdx == 0 || stateIdx == this->stateCount - 1) {
+						matrix->data()[1][idx] = 0.0f;
+						matrix->data()[2][idx] = 0.0f;
+					}
+				}
+
+				return matrix;
+
+			};
+
+		public:
+			/**
+			 * Determina se a quantidade de estados é um quadrado perfeito.<br />
+			 *
+			 * @param stateCount
+			 *					o <code>size_t</code> que representa a quantidade de estados
+			 * @param root
+			 *					o <code>size_t</code> que representa a raiz
+			 *
+			 * @return <code>true</code> caso a quantidade de estados seja um quadrado perfeito; do contrário <code>false</code>
+			 */
+			PUBLIC static bool isPerfectSquare(IN size_t stateCount, IN size_t *root) {
+				
+				*root = 0;
+				size_t temp = (*root) * (*root);
+				
+				while (temp < stateCount) {
+					(*root) = (*root) + 1;
+					temp = (*root) * (*root);
+				}
+
+				return (temp == stateCount);
+
+			};
+
+			/**
+			 * Construtor.<br />
+			 *
+			 * @param stateCount
+			 *					o <code>size_t</code> que representa a quantidade de estados
+			 * @param initalizeStates
+			 *					o <code>bool</code> que indica se os estados devem ser inicializados
+			 */
+			SmallGridWorld(IN size_t stateCount, IN bool initalizeStates = true) {
+
+				size_t root = 0;
+				if (!isPerfectSquare(stateCount, &root)) {
+					throw new IllegalParameterException();
+				}
+				
+				this->rows = root;
+				this->stateCount = stateCount;
+
+				if (initalizeStates) {
+					this->states = new std::vector<SmallGridWorldState*>(this->stateCount);
+					for (size_t stateIdx = 0; stateIdx < this->stateCount; stateIdx++) {
+						this->states->at(stateIdx) = new SmallGridWorldState(stateIdx, buildNextStatesCompleteInfo(stateIdx));
+					}
+				}
+
+			};
+
+			/**
+			 * Destrutor.<br />
+			 */
+			~SmallGridWorld() {
+
+				if (Utils::isValidHandle(this->states)) {
+					delete this->states;
+				}
+	
+			};
+
+			/**
+			 * Clona um <i>small grid world</i>.<br />
+			 *
+			 * @return o SmallGridWorld que representa o clone
+			 */
+			PUBLIC SmallGridWorld *clone() {
+
+				SmallGridWorld *clone = new SmallGridWorld(this->stateCount, false);
+				clone->states = new std::vector<SmallGridWorldState*>(this->stateCount);
+				for (size_t stateIdx = 0; stateIdx < this->stateCount; stateIdx++) {
+					clone->states->at(stateIdx) = this->states->at(stateIdx)->clone();
+				}
+				return clone;
+
+			};
+
+			/**
+			 * Obtém a quantidade de linhas do <i>grid</i>.<br />
+			 *
+			 * @return o <code>size_t</code> que representa a quantidade de linhas
+			 */
+			PUBLIC size_t getRows() {
+				return this->rows;
+			};
+
+			/**
+			 * Obtém a quantidade de estados do <i>grid</i>.<br />
+			 *
+			 * @return o <code>size_t</code> que representa a quantidade de estados
+			 */
+			PUBLIC size_t getStateCount() {
+				return this->stateCount;
+			};
+
+			/**
+			 * Obtém a um estados do <i>grid</i>.<br />
+			 *
+			 * @return o SmallGridWorldState que representa o estado
+			 */
+			PUBLIC SmallGridWorldState *getState(IN size_t stateIdx) {
+				
+				if (stateIdx > this->stateCount - 1) {
+					throw new IllegalParameterException();
+				}
+
+				return this->states->at(stateIdx);
+
+			};
+
+			/**
+			 * Imprime detalhes sobre o <i>grid</i>.
+			 */
+			PUBLIC void dump() {
+
+				Logger::log("Imprimindo detalhes sobre o grid...\n");
+				for (size_t stateIdx = 0; stateIdx < this->stateCount; stateIdx++) {
+					this->states->at(stateIdx)->dump();
+				}
+			};
+
+		}; /* class SmallGridWorld */
+
+	} /* namespace week6 */
+
+} /* namespace pel208 */
+
+#endif /* __SMALL_GRID_WORLD_H__ */
---
+++ pel208/week6/inc/SmallGridWorldState.h
@@ -0,0 +1,230 @@
+#pragma once
+
+#ifndef __SMALL_GRID_WORLD_STATE_H__
+#define __SMALL_GRID_WORLD_STATE_H__
+
+#include <inc\Utils.h>
+#include <inc\ex\IllegalParameterException.h>
+
+#include <inc\Matrix.h>
+
+#define ALLOWED_ACTIONS_QTY							4
+
+#define NEXT_STATE_COMPLETE_INFO_MATRIX_ROWS		3
+#define NEXT_STATE_IDX								0
+#define TRANSITION_PROBABILITY_IDX					1
+#define TRANSITION_REWARD_IDX						2
+
+// a probabilidade padrão de transição
+#define DEFAULT_TRANSITION_PROBABILITY				0.25f
+
+// as recompensas
+#define ACTION_REWARD								-1.0f
+#define END_STATE_REWARD							0.0f
+
+using namespace pel208::commons;
+
+namespace pel208 {
+
+	namespace week6 {
+
+		/**
+		 * Classe que representa um estado do <i>small grid world</i>.<br />
+		 *
+		 * @author arrebola
+		 */
+		class SmallGridWorldState {
+
+		private:
+			size_t stateIdx;
+			Matrix *nextStatesCompleteInfo;
+			Matrix *nextStates;
+			Matrix *transitionProbabilities;
+			Matrix *transitionRewards;
+
+			double stateValue_V;
+
+			/**
+			 * Atualiza uma célula de uma matriz.<br />
+			 *
+			 * @param matrix
+			 *					o Matrix a ser atualizada
+			 * @param rowIdx
+			 *					o índice da linha
+			 * @param colIdx
+			 *					o índice da coluna
+			 * @param value
+			 *					o valor
+			 */
+			PRIVATE static void updateMatrix(IN Matrix *matrix, IN size_t rowIdx, IN size_t colIdx, IN double value) {
+				matrix->data()[rowIdx][colIdx] = value;
+			}
+
+		public:
+			/**
+			 * Construtor.<br />
+			 *
+			 * @para stateIdx
+			 *					o <code>size_t</code> que representa o estado
+			 * @param nextStatesCompleteInfo
+			 *					o Matrix de informações sobre os próximos estados
+			 */
+			SmallGridWorldState(IN size_t stateIdx, Matrix *nextStatesCompleteInfo) {
+				
+				if (Utils::isInvalidHandle(nextStatesCompleteInfo)) {
+					throw new IllegalParameterException();
+				}
+
+				if (nextStatesCompleteInfo->getRows() != NEXT_STATE_COMPLETE_INFO_MATRIX_ROWS || 
+							nextStatesCompleteInfo->getColumns() != ALLOWED_ACTIONS_QTY) {
+					throw new IllegalParameterException();
+				}
+
+				this->stateIdx = stateIdx;
+				this->nextStatesCompleteInfo = nextStatesCompleteInfo->clone();
+
+				// inicializa as matrizes auxiliares, que serão atualizadas conforme o algoritmo
+				this->nextStates = new Matrix(1, ALLOWED_ACTIONS_QTY);
+				this->transitionProbabilities = new Matrix(1, ALLOWED_ACTIONS_QTY, DEFAULT_TRANSITION_PROBABILITY);
+				this->transitionRewards = new Matrix(1, ALLOWED_ACTIONS_QTY, ACTION_REWARD);
+				for (size_t colIdx = 0; colIdx < ALLOWED_ACTIONS_QTY; colIdx++) {
+					this->nextStates->data()[0][colIdx] = this->nextStatesCompleteInfo->data()[0][colIdx];
+					this->transitionProbabilities->data()[0][colIdx] = this->nextStatesCompleteInfo->data()[1][colIdx];
+					this->transitionRewards->data()[0][colIdx] = this->nextStatesCompleteInfo->data()[2][colIdx];
+				}
+
+				this->stateValue_V = 0.0f;
+
+			};
+
+			/**
+			 * Destrutor.<br />
+			 */
+			~SmallGridWorldState() {
+
+				if (Utils::isValidHandle(this->nextStates)) {
+					delete this->nextStates;
+				}
+
+				if (Utils::isValidHandle(this->transitionProbabilities)) {
+					delete this->transitionProbabilities;
+				}
+				
+				if (Utils::isValidHandle(this->transitionRewards)) {
+					delete this->transitionRewards;
+				}
+				
+				if (Utils::isValidHandle(this->nextStatesCompleteInfo)) {
+					delete this->nextStatesCompleteInfo;
+				}
+
+			};
+
+			/**
+			 * Clona um estado.<br />
+			 *
+			 * @return o SmallGridWorldState que representa o estado
+			 */
+			PUBLIC SmallGridWorldState *clone() {
+				
+				SmallGridWorldState *clone = new SmallGridWorldState(this->stateIdx, this->nextStatesCompleteInfo);
+				clone->setValue(this->getValue());
+				clone->updateTransitionProbabilities(this->transitionProbabilities);
+				return clone;
+
+			};
+
+			/**
+			 * Obtém o valor V de um estado.<br />
+			 *
+			 * @return o <code>double</code> que representa o valor
+			 */
+			PUBLIC double getValue() {
+				return this->stateValue_V;
+			};
+
+			/**
+			 * Configura o valor V de um estado.<br />
+			 *
+			 * @param value
+			 *					o <code>double</code> que representa o valor
+			 */
+			PUBLIC void setValue(IN double value) {
+				this->stateValue_V = value;
+			};
+
+			/**
+			 * Obtém a matriz de próximos estados.<br />
+			 *
+			 * @return o Matrix de informações sobre os próximos estados
+			 */
+			PUBLIC Matrix *getNextStates() {
+				return this->nextStates;
+			};
+			
+			/**
+			 * Obtém a matriz de probabilidades de transição para os próximos estados.<br />
+			 *
+			 * @return o Matrix de probabilidades de transição para os próximos estados
+			 */
+			PUBLIC Matrix *getTransitionProbabilities() {
+				return this->transitionProbabilities;
+			};
+
+			/**
+			 * Obtém a matriz de recompensas de transição para os próximos estados.<br />
+			 *
+			 * @return o Matrix de recompensas de transição para os próximos estados
+			 */
+			PUBLIC Matrix *getTransitionRewards() {
+				return this->transitionRewards;
+			};
+			
+			/**
+			 * Atualiza a matriz de probabilidades de transição para os próximos estados.<br />
+			 *
+			 * @param matrix
+			 *					o Matrix de probabilidades de transição para os próximos estados
+			 */
+			PUBLIC void updateTransitionProbabilities(IN Matrix *matrix) {
+
+				if (Utils::isInvalidHandle(matrix)) {
+					throw new IllegalParameterException();
+				}
+
+				size_t rows = matrix->getRows();
+				size_t columns = matrix->getColumns();
+
+				if (rows != this->transitionProbabilities->getRows() || 
+						columns != this->transitionProbabilities->getColumns()) {
+					throw new IllegalParameterException();
+				}
+
+				for (size_t idx = 0; idx < ALLOWED_ACTIONS_QTY; idx++) {
+					updateMatrix(this->transitionProbabilities, 0, idx, matrix->data()[0][idx]);
+				}
+			};
+
+			/**
+			 * Imprime detalhes sobre o estado.
+			 */
+			PUBLIC void dump() {
+
+				Logger::log("Imprimindo detalhes sobre o estado %d...\n", stateIdx);
+				Logger::log(">> V = %5.2f\n", this->getValue());
+				for (size_t idx = 0; idx < ALLOWED_ACTIONS_QTY; idx++) {
+					Logger::log("   >> P(%02d,%s,%02d) = %.2f\n", 
+						this->stateIdx, 
+						(idx == 0 ? "U" : ((idx == 1) ? "D" : ((idx == 2) ? "L" : "R"))), 
+						(size_t)this->nextStates->data()[0][idx], this->transitionProbabilities->data()[0][idx]);
+				}
+
+			};
+
+		}; /* class SmallGridWorldState */
+
+	} /* namespace week6 */
+
+} /* namespace pel208 */
+
+#endif /* __SMALL_GRID_WORLD_STATE_H__ */
